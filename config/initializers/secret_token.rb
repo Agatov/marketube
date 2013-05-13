1217deed5067e4102f7fad0fc9798d97e648e4ac
agatov <agatovs@gmail.com>
2013-05-13 14:40:21 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoMarketing::Application.config.secret_token = 'f555be7fb45ec1b353537cebb533a9dc6fcbc31488bab96455c7b0acf0159063f9593047a1b5f06412a89e9e374085780d390aeb677baf1732dbe9854f76908f'
